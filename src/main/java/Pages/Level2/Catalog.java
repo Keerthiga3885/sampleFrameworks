1137863324e2cd97cc3ab562c1f7c330a49548b1
keerthiga <keerthisri3885@gmail.com>
2024-06-12 18:33:47 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/Pages/Level2/Catalog.java
+++ src/main/java/Pages/Level2/Catalog.java
@@ -1,6 +1,5 @@
 package Pages.Level2;
 
-import Base.BaseLevel1;
 import Base.BaseLevel2;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -47,63 +46,6 @@ public class Catalog extends BaseLevel2 {
     }
 
     public void verifyCatalogNameDisableInSettings() {
-
-        if (chkCatalogName.isEnabled()) {
-
-            throw new RuntimeException("User is allowed to hide catalog name in catalog table");
-
-        } else {
-
-            System.out.println("catalog name is disabled in settings");
-
-        }
-
-    }
-
-}
-package Pages.Level2;
-
-import Base.BaseLevel2;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-public class Catalog extends BaseLevel2 {
-    @FindBy(xpath = "//a[text()='plt-beta']")
-    WebElement lnkworkspace;
-
-    @FindBy(id = "Connectivity")
-    WebElement btnConnectivity;
-
-    @FindBy(id = "OpenBook")
-    WebElement btnCatalog;
-
-    @FindBy(id = "settings")
-    WebElement btnSettings;
-
-    @FindBy(id = "name")
-    WebElement chkCatalogName;
-
-    public Catalog() {
-
-        PageFactory.initElements(driver, this);
-
-    }
-
-
-    public void verifyCatalogSettings() {
-
-        waitToClick(driver, 10, lnkworkspace);
-        lnkworkspace.click();
-
-        waitToClick(driver, 10, btnConnectivity);
-        btnConnectivity.click();
-
-        waitToClick(driver, 10, btnCatalog);
-        btnCatalog.click();
-
-        waitToClick(driver, 10, btnSettings);
-        btnSettings.click();
 
         if (chkCatalogName.isEnabled()) {
 
